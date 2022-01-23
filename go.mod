6772e3e4e960fa42899d996e5a778e9d6c8411be
564612647@qq.com <564612647@qq.com>
2022-01-24 07:43:52 +0800

    init code

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gin_detail_list
 
 go 1.15
 
-require github.com/gin-gonic/gin v1.7.7
+require (
+	github.com/gin-gonic/gin v1.7.7
+	github.com/jinzhu/gorm v1.9.16 // indirect
+)
--- main.go
+++ main.go
@@ -2,41 +2,130 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"net/http"
 )
 
+var (
+	DB *gorm.DB
+)
+
+// Todo Model
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
+func initMySQL() (err error) {
+	dns := "caocong:cc1764..@(127.0.0.1:3306)/db1?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open("mysql", dns)
+	if err != nil {
+		return
+	}
+	return DB.DB().Ping()
+}
+
 func main() {
-	r := gin.Default() // 返回默认的路由引擎
+	// 创建数据库
+	// sql: create Database bubble;
+	// 连接数据库
+	err := initMySQL()
+	if err != nil {
+		panic(err)
+	}
+	// 关闭连接
+	defer DB.Close()
 
-	//// 指定用户 GET 请求访问 /book
-	//r.GET("/book", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": "Get Book",
-	//	})
-	//})
-	//
-	//// 指定用户 PUT 请求访问 /hello
-	//r.PUT("/book", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": "Put Book",
-	//	})
-	//})
-	//
-	//// 指定用户 POST 请求访问 /hello
-	//r.POST("/book", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": "POST Book",
-	//	})
-	//})
-	//
-	//// 指定用户 DELETE 请求访问 /hello
-	//r.DELETE("/book", func(c *gin.Context) {
-	//	c.JSON(http.StatusOK, gin.H{
-	//		"message": "DELETE Book",
-	//	})
-	//})
+	// 绑定模型
+	DB.AutoMigrate(&Todo{})
 
-	
+	r := gin.Default()
+	// 加载模板文件
+	r.LoadHTMLGlob("templates/*")
+	// 加载模板文件引用的静态文件
+	r.Static("/static", "static")
 
-	// 运行
-	r.Run("127.0.0.1:9090")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	// V1
+	v1Group := r.Group("v1")
+	{
+		// 代办事项
+		// 添加
+		v1Group.POST("/todo", func(c *gin.Context) {
+			// 前端页面填写代办事项, 会发送请求到这
+			// 1. 从请求中拿到数据
+			var todo Todo
+			c.BindJSON(&todo)
+			// 2. 存入数据库
+			// 3. 返回响应
+			if err = DB.Create(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todo)
+				//c.JSON(http.StatusOK, gin.H{
+				//	"code": 200,
+				//	"msg":  "success",
+				//	"data": todo,
+				//})
+			}
+		})
+
+		// 查看
+		v1Group.GET("/todo", func(c *gin.Context) {
+			// 1. 从数据库获取清单数据
+			// 2. 返回给前端
+			var todoList []Todo
+			if err = DB.Find(&todoList).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todoList)
+			}
+		})
+
+		// 修改
+		v1Group.PUT("/todo/:id", func(c *gin.Context) {
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error": "无效的ID"})
+				return
+			}
+
+			var todo Todo
+			if err = DB.Where("id=?", id).Find(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+				return
+			}
+
+			c.BindJSON(&todo)
+			if err = DB.Save(&todo).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, todo)
+			}
+		})
+
+		// 删除
+		v1Group.DELETE("/todo/:id", func(c *gin.Context) {
+			// 1. 获取要删除数据的ID
+			id, ok := c.Params.Get("id")
+			if !ok {
+				c.JSON(http.StatusOK, gin.H{"error": "无效的ID"})
+				return
+			}
+
+			// 2. 删除数据
+			if err = DB.Where("id=?", id).Delete(Todo{}).Error; err != nil {
+				c.JSON(http.StatusOK, gin.H{"error": err.Error()})
+			} else {
+				c.JSON(http.StatusOK, gin.H{id: "deleted"})
+			}
+		})
+	}
+
+	r.Run()
 }
